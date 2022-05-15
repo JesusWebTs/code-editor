4c0947e82325c67f343cf6054689895984d9ad83
xzotacx <jesus.152006@gmail.com>
2022-05-15 17:39:23 -0400

    agregando condiciones iniciales

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ import appInit from "./modules/app.js";
 import { manageLocation, decoderLanguages } from "./modules/helpers/index.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-    let { getQuery } = manageLocation();
-    let { decoder } = decoderLanguages();
-    let initJs = "",
-        initCss = "",
-        initHtml = "";
+  let { getQuery } = manageLocation();
+  let { decoder } = decoderLanguages();
+  let initJs = "",
+    initCss = `body{\n   color: white;\n   background-color: black;\n   display: grid;\n   place-items: center;\n   height: 100vh;\n   overflow: hidden;\n};cls`,
+    initHtml = "<h1>Jesus Web</h1>";
 
-    if (getQuery("data").data) {
-        let { css, html, js } = decoder({ encripted: getQuery("data").data });
+  if (getQuery("data").data) {
+    let { css, html, js } = decoder({ encripted: getQuery("data").data });
 
-        initJs = js;
-        initCss = css;
-        initHtml = html;
-    }
-    appInit({ appId: "app", initJs, initCss, initHtml });
+    initJs = js;
+    initCss = css;
+    initHtml = html;
+  }
+  appInit({ appId: "app", initJs, initCss, initHtml });
 });
