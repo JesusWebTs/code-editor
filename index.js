e40fdca4592c6893f3bf708283cfd5ba697e37f5
xzotacx <jesus.152006@gmail.com>
2022-05-15 17:41:27 -0400

    agregando tilde a jesus

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ document.addEventListener("DOMContentLoaded", () => {
   let { getQuery } = manageLocation();
   let { decoder } = decoderLanguages();
   let initJs = "",
-    initCss = `body{\n   color: white;\n   background-color: black;\n   display: grid;\n   place-items: center;\n   height: 100vh;\n   overflow: hidden;\n};cls`,
+    initCss = `body{\n   color: white;\n   background-color: black;\n   display: grid;\n   place-items: center;\n   height: 100vh;\n   overflow: hidden;\n};`,
     initHtml = "<h1>Jesús Web</h1>";
 
-  if (getQuery("data").data) {
-    let { css, html, js } = decoder({ encripted: getQuery("data").data });
+    if (getQuery("data").data) {
+        let { css, html, js } = decoder({ encripted: getQuery("data").data });
 
-    initJs = js;
-    initCss = css;
-    initHtml = html;
-  }
-  appInit({ appId: "app", initJs, initCss, initHtml });
+        initJs = js;
+        initCss = css;
+        initHtml = html;
+    }
+    appInit({ appId: "app", initJs, initCss, initHtml });
 });
